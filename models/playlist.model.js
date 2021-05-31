fa809722eb313e7f39268493091c0834d7b04fad
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-31 22:18:16 +0530

    Fix: fixed ref to video for playlist model

--- models/playlist.model.js
+++ models/playlist.model.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const playlistSchema = new Schema({
     playlistId: { type: String , required: true  },
     playlistName: { type : String , required: true },
-    videos: [{ type: Schema.Types.ObjectId , ref: 'video' }] // have reference to the video collection, not only store the ids
+    _id : { type: Schema.Types.ObjectId , ref: 'video' }
 }, { timestamps: true});
 
 const playlist = model('playlist' , playlistSchema)
