e2d8f978ce68e8543fa0192b168466af453cb720
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-06-01 20:23:12 +0530

    Add playlist model
    Add mongoose model for playlists

--- models/playlist.model.js
+++ models/playlist.model.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose;
 
 const playlistSchema = new Schema({
-    playlistId: { type: String , required: true  },
-    playlistName: { type : String , required: true },
-    _id : { type: Schema.Types.ObjectId , ref: 'video' }
+    playlistName: { type : String , required: true , unique : true , lowercase: true , trim: true },
+    videos : [{ type: Schema.Types.ObjectId , ref: 'video' }]
 }, { timestamps: true});
 
 const playlist = model('playlist' , playlistSchema)
